bbd4c778decb706ef47a8686d6f17a4b45076b7b
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-06-16 00:52:20 -0500

    actualizado

---
+++ condicionales-3/app3.js
@@ -0,0 +1,11 @@
+var edad = 15
+var dias, horas =0
+var nombre= "john"
+
+    if( edad>=18){
+        dias= edad * 365
+        console.log(nombre + " ha vivido " + dias)
+    }
+    else{
+        console.log(nombre + " hola")
+    }
